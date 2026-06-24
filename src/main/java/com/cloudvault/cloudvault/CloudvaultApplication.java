75181bd0bec407fce8b6efca545c335e7ab03cc9
Maria Eduarda Ferreira Magalhães <mariaeduardamefm2000@gmail.com>
2026-06-24 16:09:16 -0300

    primeiro commit - estrutura inicial do CloudVault

---
+++ src/main/java/com/cloudvault/cloudvault/CloudvaultApplication.java
@@ -0,0 +1,13 @@
+package com.cloudvault.cloudvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudvault/cloudvault/CloudvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudvault.cloudvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
